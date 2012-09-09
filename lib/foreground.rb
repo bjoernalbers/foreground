7fd8d72912a8812060b909af44072d4c500ff3c4
Björn Albers <bjoernalbers@googlemail.com>
2012-09-09 14:15:51 +0200

    Add logger function

--- lib/foreground.rb
+++ lib/foreground.rb
@@ -1,8 +1,23 @@
+require 'logger'
 require 'mixlib/cli'
 require 'foreground/version'
 require 'foreground/daemon'
 require 'foreground/cli'
 
 module Foreground
-  # Your code goes here... or not.
+  def logger
+    @logger ||= create_logger
+  end
+
+  private
+
+  def create_logger
+    STDOUT.sync = true
+    l = Logger.new(STDOUT)
+    #TODO: Test this!
+    l.formatter = proc do |severity, datetime, progname, msg|
+      "#{progname} [#{severity}]: #{msg}\n"
+    end
+    l
+  end
 end
